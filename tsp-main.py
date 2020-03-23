659d5f3d3f7b9b4fbb16ae387a18dbc9a066de08
agillies8 <andrew.gillies@gmail.com>
2020-03-23 15:44:03 -0700

    comments added

--- tsp-main.py
+++ tsp-main.py
@@ -87,4 +87,105 @@ def matingPool(population, selectionResults): #just generates the actual mating 
         matingpool.append(population[index])
     return matingpool
 
+#breeding - need to visit every city at least once, remember. need 'ordered crossover' see fig in medium post. pick subsegment of parent 1, put in same location, then fill in rest in order from 2nd parent, skipping pre selected cities
+def breed(parent1, parent2):
+    child = []
+    childP1 = []
+    childP2 = []
+
+    geneA = int(random.random()* len(parent1)) #this could be done better
+    geneB = int(random.random()*len(parent1))
+
+    startGene = min(geneA, geneB)
+    endGene = max(geneA, geneB)
+
+    for i in range(startGene, endGene):
+        childP1.append(parent1[i])
+    
+    childP2 = [item for item in parent2 if item not in childP1] #look into whats happening here
+
+    child = childP1 + childP2
+    return child
+
+def breedPopulation(matingpool, eliteSize):
+    children = []
+    length = len(matingpool) - eliteSize #sets array size for new children, less our elite that we will pass on
+    pool = random.sample(matingpool, len(matingpool)) #randomly arrange parents from mating pool into pool
+
+    for i in range(0,eliteSize): #pass thru the elite parents
+        children.append(matingpool[i])
+
+    for i in range(0,length): # breed everyone and fill out children using length above
+        child = breed(pool[i], pool[len(matingpool)-i-1])
+        children.append(child)
+    return children
+
+#swap mutation -this function is for individual. Note: cant just swtich, need to maintain list of all cities
+def mutate(individual, mutationRate):
+    for swapped in range(len(individual)): #cycle thru cities in the individual route
+        if(random.random()< mutationRate): #if porbability is met, do the below and swap cities
+            swapWith = int(random.random()*len(individual)) #pick random location to do the swap
+
+            city1 = individual[swapped]
+            city2 = individual[swapWith]
+
+            individual[swapped] = city2
+            individual[swapWith] = city1
+return individual
+
+#cycle thru population and perform individual mutation function on each one
+def mutatePopulation(population, mutationRate):
+    mutatedPop=[]
+
+    for ind in range(0, len(population)):
+        mutatedInd = mutate(population[ind], mutationRate)
+        mutatedPop.append(mutatedInd)
+    return mutatedPop
+
+def nextGeneration(currentGen, eliteSize, mutationRate):
+    popRanked = rankRoutes(currentGen)
+    selectionResults = selection(popRanked, eliteSize)
+    matingpool = matingPool(currentGen, selectionResults)
+    children  =breedPopulation(matingpool, eliteSize)
+    nextGeneration = mutatePopulation(children, mutationRate)
+    return nextGeneration
+
+def geneticAlgorithm(population, popSize, eliteSize, mutationRate, generations): #to get the best route output
+    pop = initialPopulation(popSize, population)
+    print("initial distance: " + str(1/rankRoutes(pop)[0][1]))
+
+    for i in range(0, generations):
+        pop = nextGeneration(pop,eliteSize, mutationRate)
+
+    print("Final distance: " = str(1 / rankRoutes(pop)[0][1]) )
+    bestRouteIndex = rankRoutes(pop)[0][0]
+    bestRoute = pop[bestRouteIndex]
+    return bestRoute
+
+def geneticAlgorithmPlot(population, popSize, eliteSize, mutationRate, generations): #if you want to plot the results
+    pop = initialPopulation(popSize, population)
+
+    progress = []
+    progress.append(1/rankRoutes(pop)[0][1])
+
+    for i in range(0, generations):
+        pop = nextGeneration(pop,eliteSize, mutationRate)
+        progress.append(1/rankRoutes(pop)[0][1])
+
+    plt.plot(progress)
+    plt.ylabel('Distance')
+    plt.xlabel('Generation')
+    plt.show
+
+
+
+cityList = []
+
+for i in range(0,25):
+    cityList.append(City(x=int(random.random()*200), y=int(random.random()*200)))
+
+geneticAlgorithm(population=cityList, popSize=100, eliteSize=20, mutationRate=0.01, generations=500)
+
+
+
 
